64d738cb5e95cd2c549380a6d37ff5caff6e4519
vadim-and <162820240+vadim-and@users.noreply.github.com>
2024-04-11 21:09:18 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import string
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+password_length = int(input("Введите желаемую длину пароля: "))
+generated_password = generate_password(password_length)
+print("Ваш сгенерированный пароль:", generated_password)
